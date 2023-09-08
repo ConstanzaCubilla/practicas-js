95d51b2c57fab7b1aba54de820a91ae43a56e8e1
ConstanzaCubilla <142610063+ConstanzaCubilla@users.noreply.github.com>
2023-09-08 11:18:11 -0400

    ejer2.js

--- ejer2.js
+++ ejer2.js
@@ -1,6 +1,6 @@
 let i=Math.floor(Math.random()*(360));
 console.log("Posición o ángulo Inicial "+ i);
-vAleatorio=Math.floor(Math.random()*((i+360*10)-(i+5*360)+1)+(i+5*360));//the max and min depend on the initial value
+vAleatorio=Math.floor(Math.random()*((i+360*10)-(i+5*360))+1)+(i+5*360));//the max and min depend on the initial value
 console.log("Valor Aleatorio Generado en Grados: "+ vAleatorio);
 angulo=vAleatorio%360;
 console.log("Ángulo Final: "+angulo);
